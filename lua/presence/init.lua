a68a861422e1a537def21c5286bd2645153dc805
Chika <95551108+chikaof@users.noreply.github.com>
2023-04-09 03:29:43 +0200

    some changes

--- lua/presence/init.lua
+++ lua/presence/init.lua
@@ -116,10 +116,10 @@ function Presence:setup(...)
 
     -- General options
     self:set_option("auto_update", 1)
-    self:set_option("client_id", "793271441293967371")
+    self:set_option("client_id", "1094423985653031003")
     self:set_option("debounce_timeout", 10)
     self:set_option("main_image", "neovim")
-    self:set_option("neovim_image_text", "The One True Text Editor")
+    self:set_option("neovim_image_text", "Neovim")
     self:set_option("enable_line_number", false)
     -- Status text options
     self:set_option("editing_text", "Editing %s")
